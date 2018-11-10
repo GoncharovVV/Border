39d1e5e309b986e917b5ae8f18846549c71046d3
GoncharovVV <goncharov.v.v2017@gmail.com>
2018-11-10 09:55:20 +0300

    Modifies

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     initCustomSelect();
     initAddFormInput();
+    initAlbum();
+    initToggleBlock();
 });
 
 function initCustomSelect() {
@@ -24,4 +26,47 @@ function initAddFormInput() {
 
         initCustomSelect();
     });
+}
+
+function initAlbum() {
+    $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+}
+
+function initToggleBlock() {
+    $('.js-show-description').on('click',showBlock);
+    function hideBlock(){
+        $('.js-hide-description').click(function () {
+            console.log('1');
+            var selector = $(this).attr('data-block');
+            $(this).addClass('js-show-description').removeClass('js-hide-description');
+            $(this).prev(selector).animate({maxHeight : $(this).attr('data-current')},100);
+            $('.js-show-description').on('click',showBlock);
+        });
+    }
+    function showBlock() {
+        var selector = $(this).attr('data-block'),
+            targetHeight = $(this).prev(selector).prop('scrollHeight') + 'px',
+            currentHeight = $(this).prev(selector).css('height');
+        console.log('0');
+        $(this).addClass('js-hide-description').removeClass('js-show-description').attr('data-current',currentHeight);
+        $(this).prev(selector).animate({maxHeight : targetHeight},100);
+        $(this).off('click',showBlock);
+        hideBlock();
+    }
 }
