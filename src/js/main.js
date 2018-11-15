394fc63e897ac6f1b81370226b3b540747a02c23
GoncharovVV <goncharov.v.v2017@gmail.com>
2018-11-15 20:46:30 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     initAddFormInput();
     initAlbum();
     initToggleBlock();
+    initToggleMenu();
+    scrollToSection();
 });
 
 function initCustomSelect() {
@@ -30,43 +32,79 @@ function initAddFormInput() {
 
 function initAlbum() {
     $('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			}
-		}
-	});
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
 }
 
 function initToggleBlock() {
-    $('.js-show-description').on('click',showBlock);
-    function hideBlock(){
-        $('.js-hide-description').click(function () {
-            console.log('1');
-            var selector = $(this).attr('data-block');
-            $(this).addClass('js-show-description').removeClass('js-hide-description');
-            $(this).prev(selector).animate({maxHeight : $(this).attr('data-current')},100);
-            $('.js-show-description').on('click',showBlock);
-        });
-    }
+    $('.js-show-description').on('click', showBlock);
+
     function showBlock() {
         var selector = $(this).attr('data-block'),
             targetHeight = $(this).prev(selector).prop('scrollHeight') + 'px',
             currentHeight = $(this).prev(selector).css('height');
-        console.log('0');
-        $(this).addClass('js-hide-description').removeClass('js-show-description').attr('data-current',currentHeight);
-        $(this).prev(selector).animate({maxHeight : targetHeight},100);
-        $(this).off('click',showBlock);
-        hideBlock();
+
+        if ($(this).attr('data-min')) {
+            if (parseInt(currentHeight) < parseInt(targetHeight)) {
+                $(this).addClass('open').prev(selector).animate({
+                    maxHeight: targetHeight
+                }, 100);
+            } else {
+                $(this).removeClass('open').prev(selector).animate({
+                    maxHeight: $(this).attr('data-min')
+                }, 100);
+            }
+        } else {
+            $(this).addClass('open').attr('data-min', currentHeight).prev(selector).animate({
+                maxHeight: targetHeight
+            }, 100);
+        }
     }
+}
+
+function initToggleMenu() {
+    $('.js-toggle').on('click', showBlock);
+
+    function showBlock() {
+        var thatClass = $(this).attr('data-class'),
+            targetNode = $($(this).attr('data-parent'));
+        $('[data-class]').each(function () {
+            var checkVal = $(this).attr('data-class');
+            if ((targetNode).hasClass(checkVal) && checkVal !== thatClass) {
+                targetNode.removeClass(checkVal);
+            }
+        });
+
+        if ((targetNode).hasClass(thatClass)) {
+            targetNode.removeClass(thatClass);
+        } else {
+            targetNode.addClass(thatClass);
+        }
+    }
+
+    function checkPrevios() {
+
+    }
+}
+function scrollToSection() {
+    $(".nav__link").click(function (){
+        var targetId = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(targetId).offset().top
+        }, 1000);
+    });
 }
